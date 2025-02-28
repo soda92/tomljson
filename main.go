a6aa3d52878defc136e0f7c1033431fcbd74cf26
sodacris <wjxa20152015@gmail.com>
2025-02-28 16:01:23 +0800

    split testtoml

--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/pelletier/go-toml/v2"
-
 	"flag"
 )
 
-type MyConfig struct {
-	Version int
-	Name    string
-	Tags    []string
-}
-
-func testToml() {
-	doc := `
-	version = 2
-	name = "go-toml"
-	tags = ["go", "toml"]
-	`
-
-	var cfg MyConfig
-	err := toml.Unmarshal([]byte(doc), &cfg)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("version:", cfg.Version)
-	fmt.Println("name:", cfg.Name)
-	fmt.Println("tags:", cfg.Tags)
-}
-
 func main() {
 	testtoml := flag.Bool("testtoml", false, "test toml parse")
+	flag.Parse()
 	if *testtoml {
 		testToml()
 	}
 
 	config := MustParseJson("config.json")
 	MustSaveJson(config, "config2.json")
+	MustSaveToml(config, "config.toml")
 }
